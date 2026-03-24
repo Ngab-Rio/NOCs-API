3df6a992f5b63f117ef452aa742642fa6eb8e593
Ngab-Rio <mamanadifirmansyah@gmail.com>
2026-03-24 14:51:28 +0700

    setup connection database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,21 @@
-package server
+package main
+
+import (
+	"github.com/Ngab-Rio/NOCs-API/internal/config"
+	"github.com/Ngab-Rio/NOCs-API/internal/database"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	cfg := config.Load()
+
+	database.Connect(cfg.DBDSN())
+	router.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "API Is Running",
+		})
+	})
+
+	router.Run(":" + cfg.AppPort)
+}
