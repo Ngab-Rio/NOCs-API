3e91b22a2e560a9f42b9d0921ca9b66c94462e5d
Ngab-Rio <mamanadifirmansyah@gmail.com>
2026-03-26 16:07:38 +0700

    fix: refactor auth handler

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -1,10 +1,9 @@
 package handlers
 
 import (
-	"net/http"
-
 	"github.com/Ngab-Rio/NOCs-API/internal/dto"
 	"github.com/Ngab-Rio/NOCs-API/internal/services"
+	"github.com/Ngab-Rio/NOCs-API/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,24 +18,15 @@ func NewAuthHandler(authService services.AuthService) *AuthHandler {
 func (h *AuthHandler) Login(c *gin.Context) {
 	var req dto.LoginRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"message": "Invalid Request Body",
-			"error":   err.Error(),
-		})
+		utils.HandleError(c, err)
 		return
 	}
 
 	resp, err := h.authService.Login(c, req)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": "Internal Server Error",
-			"error":   err.Error(),
-		})
+		utils.HandleError(c, err)
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Login Success",
-		"data":    resp,
-	})
+	utils.Success(c, "Login Success", resp)
 }
